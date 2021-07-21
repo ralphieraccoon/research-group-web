f7c69069863a05f3efb5bbfa7c73421a2cd099a6
ralphieraccoon <damian.crosby@manchester.ac.uk>
2021-07-21 21:58:00 +0100

    Trying some stuff out.

--- js/bibtex.js
+++ js/bibtex.js
@@ -1,4 +1,4 @@
 function bibtex(code) {
     var opened = window.open("");
-    opened.document.write("Your HTML here");
+    opened.document.write('<html><title>BibTeX</title><body><pre style="word-wrap: break-word; white-space: pre-wrap;">' + code + "</pre></body></html>");
 }
