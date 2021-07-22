4ad61e01b25c94ef7192a8ac2ca7f188455905a5
ralphieraccoon <damian.crosby@manchester.ac.uk>
2021-07-22 22:26:59 +0100

    Modals.

--- js/bibtex.js
+++
@@ -1,4 +0,0 @@
-function bibtex(code) {
-    var opened = window.open("");
-    opened.document.write('<html><title>BibTeX</title><body><pre style="word-wrap: break-word; white-space: pre-wrap;">' + code + "</pre></body></html>");
-}
